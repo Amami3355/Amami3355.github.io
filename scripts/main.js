16a6a9fbbf3a0a9cbee2a25e18ad15aa2e190e66
Amami3355 <amamimourad13@gmail.com>
2022-09-07 08:17:55 +0200

    version 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,28 @@
 
 //-----------------------Text Areas and progress bars
-function showCssArea(){
-    let htmlEditor = document.querySelector("#html_editor");
-    let cssEditor = document.querySelector("#css_editor");
-    let jsEditor = document.querySelector("#js_editor");
-    htmlEditor.style.display = "none";
-    cssEditor.style.display = "block";
-    jsEditor.style.display  ="none";
-}
 
 function showHtmlArea(){
-    let htmlEditor = document.querySelector("#html_editor");
-    let cssEditor = document.querySelector("#css_editor");
-    let jsEditor = document.querySelector("#js_editor");
+    let htmlEditor = document.querySelector("#html_editor_container");
+    let cssEditor = document.querySelector("#css_editor_container");
+    let jsEditor = document.querySelector("#js_editor_container");
     htmlEditor.style.display = "block";
     cssEditor.style.display = "none";
     jsEditor.style.display  ="none";
 }
 
+function showCssArea(){
+    let htmlEditor = document.querySelector("#html_editor_container");
+    let cssEditor = document.querySelector("#css_editor_container");
+    let jsEditor = document.querySelector("#js_editor_container");
+    htmlEditor.style.display = "none";
+    cssEditor.style.display = "block";
+    jsEditor.style.display  ="none";
+}
+
 function showJsArea(){
-    let htmlEditor = document.querySelector("#html_editor");
-    let cssEditor = document.querySelector("#css_editor");
-    let jsEditor = document.querySelector("#js_editor");
+    let htmlEditor = document.querySelector("#html_editor_container");
+    let cssEditor = document.querySelector("#css_editor_container");
+    let jsEditor = document.querySelector("#js_editor_container");
     htmlEditor.style.display = "none";
     cssEditor.style.display = "none";
     jsEditor.style.display  ="block";
@@ -29,14 +30,14 @@ function showJsArea(){
 
 function ChangeHtmlProgressBar(){
     let htmlTextArea = document.querySelector("#html_editor");
-    
+    let lengthsHtml = document.querySelector("#lengths_html");
     let progressBar = document.querySelector("#html_progress div");
     let pourcentage = document.querySelector("#html_pourcentage");
-    progressBar.style.width = `${htmlTextArea.value.length/2}%`;
-    pourcentage.textContent = `${htmlTextArea.value.length/2}%`;
+    progressBar.style.width = `${htmlTextArea.value.length/lengthsHtml.value*100}%`;
+    pourcentage.textContent = `${htmlTextArea.value.length/lengthsHtml.value*100}%`;
     if (htmlTextArea.value.length <= 100){
         progressBar.style.backgroundColor = "green";
-    }else if ((htmlTextArea.value.length> 100) && (htmlTextArea.value.length <= 160)){
+    }else if ((htmlTextArea.value.length> (lengthsHtml.value/2)) && (htmlTextArea.value.length <= 160)){
         progressBar.style.backgroundColor = "orange";
     }else{
         progressBar.style.backgroundColor = "red";
@@ -46,10 +47,11 @@ function ChangeHtmlProgressBar(){
 
 function ChangeCssProgressBar(){
     let cssTextArea = document.querySelector("#css_editor");
+    let lengthsCss = document.querySelector("#lengths_css");
     let progressBar = document.querySelector("#css_progress div");
     let pourcentage = document.querySelector("#css_pourcentage");
-    progressBar.style.width = `${cssTextArea.value.length/2}%`;
-    pourcentage.textContent = `${cssTextArea.value.length/2}%`;
+    progressBar.style.width = `${cssTextArea.value.length/lengthsCss.value*100}%`;
+    pourcentage.textContent = `${cssTextArea.value.length/lengthsCss.value*100}%`;
     if (cssTextArea.value.length <= 100){
         progressBar.style.backgroundColor = "green";
     }else if ((cssTextArea.value.length> 100) && (cssTextArea.value.length <= 160)){
@@ -61,10 +63,11 @@ function ChangeCssProgressBar(){
 
 function ChangeJsProgressBar(){
     let jsTextArea = document.querySelector("#js_editor"); 
+    let lengthsJs = document.querySelector("#lengths_js");
     let progressBar = document.querySelector("#js_progress div");
     let pourcentage = document.querySelector("#js_pourcentage");
-    progressBar.style.width = `${jsTextArea.value.length/2}%`;
-    pourcentage.textContent = `${jsTextArea.value.length/2}%`;
+    progressBar.style.width = `${jsTextArea.value.length/lengthsJs.value*100}%`;
+    pourcentage.textContent = `${jsTextArea.value.length/lengthsJs.value*100}%`;
     if (jsTextArea.value.length <= 100){
         progressBar.style.backgroundColor = "green";
     }else if ((jsTextArea.value.length> 100) && (jsTextArea.value.length <= 160)){
@@ -82,4 +85,13 @@ function htmlBarChanged(){
         progressBar.style.color = "orange";
     else
         progressBar.style.color = "red";
+}
+
+
+
+function changeCharLengthHTML(){
+    let htmlEditor = document.querySelector("#html_editor");
+    let lengthsHtml = document.querySelector("#lengths_html");
+    htmlEditor.maxLength = lengthsHtml.value;
+
 }
